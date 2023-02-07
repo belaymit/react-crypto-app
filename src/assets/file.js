74c4836779508c1a556a6ea7e83825fb7ac60eae
belaymit <2belamit@gmail.com>
2023-02-07 10:08:22 +0300

    removed unnecessary esports

--- src/assets/file.js
+++ src/assets/file.js
@@ -1,5 +1,3 @@
 import logo from './logo.jpg';
-import cryptoCoin from './crypto-coin.jfif';
-import cryptoDisplay from './crypto-display.avif';
 
-export { logo, cryptoCoin, cryptoDisplay };
+export default logo;
