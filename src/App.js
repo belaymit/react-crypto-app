74e48259de601621116099cae47cb7ba8f6d6597
belaymit <2belamit@gmail.com>
2023-02-07 09:29:38 +0300

    fetched darkMode from store and dispatched

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Home from './pages/home/Home';
 import Details from './components/UI/Details';
 import Footer from './components/footer/Footer';
+import './darkMode/darkMode.scss';
 
-const App = () => (
-  <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/coins/:id" element={<Details />} />
-    </Routes>
-    <Footer />
-  </>
-);
+const App = () => {
+  const { mode } = useSelector((state) => state.darkMode);
+  return (
+    <div className={mode ? 'app dark' : 'app'}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins/:id" element={<Details />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
