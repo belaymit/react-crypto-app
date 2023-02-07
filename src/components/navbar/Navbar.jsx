368da75df89352209e84dd2ee015f6b2793e0193
belaymit <2belamit@gmail.com>
2023-02-07 10:07:37 +0300

    chnaged logo image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { MdOutlineLightMode } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { toggleDarkMode } from '../../redux/darkMode/darkModeSlice';
-import { logo } from '../../assets/file';
+import logo from '../../assets/file';
 import '../../styles/navbar.scss';
 
 const Navbar = (props) => {
