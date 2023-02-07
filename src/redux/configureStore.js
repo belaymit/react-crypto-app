d62f1e4226bc4ed59889c873eee0cf9caee9fe47
belaymit <2belamit@gmail.com>
2023-02-07 09:27:28 +0300

    added darkMode reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import coinReducer from './feature/coinSlice';
+import darkModeReducer from './darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
     name: 'coin',
     coin: coinReducer,
+    darkMode: darkModeReducer,
   },
 });
 
